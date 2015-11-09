c0e93e8df978238668c148657bba2d6df976350a
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 09:44:49 +0100

    Add service

--- src/proteo.ui.buscador-contacto.visor-service.js
+++ src/proteo.ui.buscador-contacto.visor-service.js
@@ -1,15 +1,16 @@
 (function () {
     'use strict';
 
-angular.module('proteo.ui.buscador-contacto.visor').service('visorService', function () {
+	angular.module('proteo.ui.buscador-contacto.visor').service('visorService', function () {
 
-/*
-      this.getFullname = function(customer){return customer.firstName+ ' ' + customer.lastName;};
-  
-      this.basePath={};
-      this.setBasePath = function(path) {basePath=path;};
-      this.getBasePath = function() { return basePath;}
-      */
-  });
+		/*
+		this.getFullname = function(customer){return customer.firstName+ ' ' + customer.lastName;};
+
+		this.basePath={};
+		this.setBasePath = function(path) {basePath=path;};
+		this.getBasePath = function() { return basePath;}
+		*/
+	
+	});
 
 }());
