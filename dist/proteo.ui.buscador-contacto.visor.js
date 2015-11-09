9e343fb5531544892189526c069af6a4ac0d0dac
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 11:42:41 +0100

    directive >> update name of param listContacts

--- dist/proteo.ui.buscador-contacto.visor.js
+++ dist/proteo.ui.buscador-contacto.visor.js
@@ -23,7 +23,7 @@
 	  return {
 	    restrict	: 	"E",
 		scope: {
-			datacontact : '=datacontact'
+			contactsFiltered : '=datacontact'
 		},
 	    controller 	: 	'buscadorContactoVisorCtrl',
 	    templateUrl : 	'./bower_components/proteo.ui.buscador-contacto.visor/dist/proteo.ui.buscador-contacto.visor.tpl.html',
