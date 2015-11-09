aa7b2e7c64ea097e74a4332974d6482618d76539
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 15:10:50 +0100

    controller add debug

--- dist/proteo.ui.buscador-contacto.visor.js
+++ dist/proteo.ui.buscador-contacto.visor.js
@@ -12,6 +12,7 @@
 
 	angular.module('proteo.ui.buscador-contacto.visor').controller('buscadorContactoVisorCtrl', function($scope,$http) {
 		console.log("buscadorContactoVisorCtrl");
+		console.log("ShowContacts >> "+JSON.stringify($scope.showContacts));
 	});
 
 }());
--- src/proteo.ui.buscador-contacto.visor-controller.js
+++ src/proteo.ui.buscador-contacto.visor-controller.js
@@ -3,6 +3,7 @@
 
 	angular.module('proteo.ui.buscador-contacto.visor').controller('buscadorContactoVisorCtrl', function($scope,$http) {
 		console.log("buscadorContactoVisorCtrl");
+		console.log("ShowContacts >> "+JSON.stringify($scope.showContacts));
 	});
 
 }());
