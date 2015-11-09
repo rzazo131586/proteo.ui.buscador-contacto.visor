10f727e4728669f706ad52a78b6ba50bfa4fdd32
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 11:14:37 +0100

    DISt

--- dist/proteo.ui.buscador-contacto.visor.js
+++ dist/proteo.ui.buscador-contacto.visor.js
@@ -12,14 +12,6 @@
 
 	angular.module('proteo.ui.buscador-contacto.visor').controller('buscadorContactoVisorCtrl', function($scope,$http) {
 		console.log("buscadorContactoVisorCtrl");
-		$http.get('http://localhost:3000/users').success(function(data) {
-            $scope.users = data;
-        });
-        setTimeout(function(){
-	        console.log("datacontact || "+$scope.datacontact);
-	        console.log("users || "+JSON.stringify($scope.users));
-        },500);
-
 	});
 
 }());
