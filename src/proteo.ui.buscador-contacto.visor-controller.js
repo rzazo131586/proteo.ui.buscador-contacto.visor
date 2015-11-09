c17f14cca690936ecc5e1c4ba6a658b21e84738d
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 09:44:31 +0100

    Add controller

--- src/proteo.ui.buscador-contacto.visor-controller.js
+++ src/proteo.ui.buscador-contacto.visor-controller.js
@@ -1,8 +1,8 @@
 (function () {
     'use strict';
 
-angular.module('proteo.ui.buscador-contacto.visor').controller('buscadorContactoVisorCtrl', function($scope,personService) {
-
-});
+	angular.module('proteo.ui.buscador-contacto.visor').controller('buscadorContactoVisorCtrl', function($scope,personService) {
+		console.log("buscadorContactoVisorCtrl");
+	});
 
 }());
