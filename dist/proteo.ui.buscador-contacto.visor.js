27ba4a4ccc2873e898d8285b9aee3c31ddee015d
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 13:13:30 +0100

    clean code params

--- dist/proteo.ui.buscador-contacto.visor.js
+++ dist/proteo.ui.buscador-contacto.visor.js
@@ -23,7 +23,7 @@
 	  return {
 	    restrict	: 	"E",
 		scope: {
-			contactsFiltered : '=showContactsList'
+			showContacts : '='
 		},
 	    controller 	: 	'buscadorContactoVisorCtrl',
 	    templateUrl : 	'./bower_components/proteo.ui.buscador-contacto.visor/dist/proteo.ui.buscador-contacto.visor.tpl.html',
