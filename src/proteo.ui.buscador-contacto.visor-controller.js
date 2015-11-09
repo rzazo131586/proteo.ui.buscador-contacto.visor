6657081e894afa8424d5d17adb137332475b251a
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 16:03:09 +0100

    clean code >> consoles

--- src/proteo.ui.buscador-contacto.visor-controller.js
+++ src/proteo.ui.buscador-contacto.visor-controller.js
@@ -3,7 +3,6 @@
 
 	angular.module('proteo.ui.buscador-contacto.visor').controller('buscadorContactoVisorCtrl', function($scope,$http) {
 		console.log("buscadorContactoVisorCtrl");
-		console.log("ShowContacts >> "+JSON.stringify($scope.showContacts));
 	});
 
 }());
