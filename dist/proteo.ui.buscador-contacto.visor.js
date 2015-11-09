3b2f052360673888de9f3253fd71080025f8e1e2
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 09:45:04 +0100

    DIST

---
+++ dist/proteo.ui.buscador-contacto.visor.js
@@ -0,0 +1,49 @@
+(function () {
+
+    'use strict';	
+
+	angular.module('proteo.ui.buscador-contacto.visor',['pascalprecht.translate'])
+		.config(['$translateProvider', function ($translateProvider) {
+			// custom code 
+	}]);
+}());
+(function () {
+    'use strict';
+
+	angular.module('proteo.ui.buscador-contacto.visor').controller('buscadorContactoVisorCtrl', function($scope,personService) {
+		console.log("buscadorContactoVisorCtrl");
+	});
+
+}());
+
+(function () {
+    'use strict';
+
+	angular.module('proteo.ui.buscador-contacto.visor').directive('buscadorContacto.visor',function() {
+	  return {
+	    restrict	: 	"E",
+		scope: {
+			datacontact : '=datacontact'
+		},
+	    controller 	: 	'buscadorContactoVisorCtrl',
+	    templateUrl : 	'./bower_components/proteo.ui.buscador-contacto.visor/dist/proteo.ui.buscador-contacto.visor.tpl.html',
+	  };
+	});
+
+}());
+(function () {
+    'use strict';
+
+	angular.module('proteo.ui.buscador-contacto.visor').service('visorService', function () {
+
+		/*
+		this.getFullname = function(customer){return customer.firstName+ ' ' + customer.lastName;};
+
+		this.basePath={};
+		this.setBasePath = function(path) {basePath=path;};
+		this.getBasePath = function() { return basePath;}
+		*/
+	
+	});
+
+}());
