368346a2434c556cc888c412e77ecd483062b579
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 09:57:27 +0100

    test connet to User service && test dataContact

--- src/proteo.ui.buscador-contacto.visor-controller.js
+++ src/proteo.ui.buscador-contacto.visor-controller.js
@@ -3,6 +3,12 @@
 
 	angular.module('proteo.ui.buscador-contacto.visor').controller('buscadorContactoVisorCtrl', function($scope,personService) {
 		console.log("buscadorContactoVisorCtrl");
+		$http.get('http://localhost:3000/users').success(function(data) {
+            $scope.users = data;
+        });
+        console.log("datacontact || "+$scope.datacontact);
+        console.log("users || "+$scope.users);
+
 	});
 
 }());
