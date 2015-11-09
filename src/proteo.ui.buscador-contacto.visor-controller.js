b78615c8090d88ca971152d35be44504667533a2
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-09 10:05:29 +0100

    add HTTP Service [Fake simulate]

--- src/proteo.ui.buscador-contacto.visor-controller.js
+++ src/proteo.ui.buscador-contacto.visor-controller.js
@@ -1,13 +1,15 @@
 (function () {
     'use strict';
 
-	angular.module('proteo.ui.buscador-contacto.visor').controller('buscadorContactoVisorCtrl', function($scope,personService) {
+	angular.module('proteo.ui.buscador-contacto.visor').controller('buscadorContactoVisorCtrl', function($scope,$http) {
 		console.log("buscadorContactoVisorCtrl");
 		$http.get('http://localhost:3000/users').success(function(data) {
             $scope.users = data;
         });
-        console.log("datacontact || "+$scope.datacontact);
-        console.log("users || "+$scope.users);
+        setTimeout(function(){
+	        console.log("datacontact || "+$scope.datacontact);
+	        console.log("users || "+JSON.stringify($scope.users));
+        },500);
 
 	});
 
