304eb6a0f469f55ac6475628405047444885e825
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-10 13:10:49 +0100

    DIST

--- dist/proteo.ui.buscador-contacto.visor-dist.js
+++ dist/proteo.ui.buscador-contacto.visor-dist.js
@@ -58,3 +58,47 @@
 
 }());
 /* END Buscador-Contacto.VISOR SERVICE */
+angular.module('proteo.ui.buscador-contacto.visor').run(['$templateCache', function($templateCache) {
+  'use strict';
+
+  $templateCache.put('src/proteo.ui.buscador-contacto.visor.tpl.html',
+    "<div class=\"buscador-personas-container\">\r" +
+    "\n" +
+    "\t<div ng-repeat=\"contact in showContacts\" class=\"col-md-12 ng-scope\">\r" +
+    "\n" +
+    "\t\t<div class=\"panel account-item\">\r" +
+    "\n" +
+    "\t\t\t<div class=\"item-body clearfix\">\r" +
+    "\n" +
+    "\t\t\t\t<div class=\"pull-left\">\r" +
+    "\n" +
+    "\t\t\t\t\t<div class=\"account-name ng-binding\">\r" +
+    "\n" +
+    "\t\t\t\t\t\t{{contact.name}}\r" +
+    "\n" +
+    "\t\t\t\t\t</div>\r" +
+    "\n" +
+    "\t\t\t\t\t<div class=\"account-holder ng-binding\">{{contact.company.name}}</div>\r" +
+    "\n" +
+    "\t\t\t\t</div>\r" +
+    "\n" +
+    "\t\t\t\t<div class=\"pull-right text-right\">\r" +
+    "\n" +
+    "\t\t\t\t\t<div class=\"account-name ng-binding\">\r" +
+    "\n" +
+    "\t\t\t\t\t\t{{contact.username}}\r" +
+    "\n" +
+    "\t\t\t\t\t</div>\r" +
+    "\n" +
+    "\t\t\t\t</div>\r" +
+    "\n" +
+    "\t\t\t</div>\r" +
+    "\n" +
+    "\t\t</div>\r" +
+    "\n" +
+    "\t</div>\r" +
+    "\n" +
+    "</div>"
+  );
+
+}]);
